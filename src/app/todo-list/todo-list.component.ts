1a13c728bebc57fa1191e9fdad3f8360750073db
Hadiza Asabe umar <dijapaul01@gmail.com>
2023-07-19 10:43:02 +0100

    first commit

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,32 @@
+
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+
+  taskArray =  [{taskName: 'Brush teeth', isCompleted: false}];
+constructor (){}
+ ngOnInit(): void {
+   
+ }
+  onSubmit(form : NgForm){
+    console.log(form)
+
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    })
+  }
+
+  onDelete(index: number){
+    console.log(index);
+
+    this.taskArray.splice(index, 1);
+  }
+}
